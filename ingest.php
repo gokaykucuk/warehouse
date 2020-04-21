6f5a394d1e3062c961ff9798317c475a19eab847
= <=>
2020-04-21 21:06:38 +0200

    added connector again

--- ingest.php
+++ ingest.php
@@ -7,14 +7,24 @@
     file_put_contents($file, file_get_contents('php://input')."\n", FILE_APPEND);
 ?>
 <script>
-    const WAREHOUSE_ENDPOINT = "localhost:8000"
-    const pushRawData = (warehouse_key, data) => {
-        fetch("/ingest.php",{
-            method: "POST",
-            headers: {
-                WAREHOUSE_KEY: warehouse_key
-            },
-            body: data.toString()
-        });
+    const WAREHOUSE_KEY = "KEY";
+    const WAREHOUSE_ENDPOINT = "URL";
+    const pushRawData = async (data, warehouse_key) => {
+        const currentKey = WAREHOUSE_KEY || warehouse_key;
+        try {
+            axios({
+                method: "post",
+                url: `${WAREHOUSE_ENDPOINT}/ingest.php`,
+                headers: {
+                    WAREHOUSE_KEY: currentKey,
+                },
+                data: data
+                    .map((c) => c && typeof c === "string" && c.replace(",", "\n"))
+                    .join(","),
+            });
+        } catch (e) {
+            console.log(e);
+        }
     };
+
 </script>
