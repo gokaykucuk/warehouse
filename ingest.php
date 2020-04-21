d065cc9146a13aa2cf9491a2d472b728dabf7b57
= <=>
2020-04-21 20:19:46 +0200

    add tostring to data

--- ingest.php
+++ ingest.php
@@ -1,7 +1,20 @@
 <?php
+    if(isset($_SERVER["HTTP_WAREHOUSE_KEY"]) == 0){
+        die("NO WAREHOUSE_KEY HEADER.");
+    }
     $file_name = $_SERVER["HTTP_WAREHOUSE_KEY"];
     $file = "$file_name.csv";
-    $data = json_decode(file_get_contents('php://input'), true);
-    $data_to_write = implode(",",array_values($data)) . "\n";
-    file_put_contents($file, $data_to_write, FILE_APPEND);
+    file_put_contents($file, file_get_contents('php://input')."\n", FILE_APPEND);
 ?>
+<script>
+    const WAREHOUSE_ENDPOINT = "localhost:8000"
+    const pushRawData = (warehouse_key, data) => {
+        fetch("/ingest.php",{
+            method: "POST",
+            headers: {
+                WAREHOUSE_KEY: warehouse_key
+            },
+            body: data.toString()
+        });
+    };
+</script>
